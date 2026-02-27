c185f33d5f9213c82668c1dfea4e9a4988f5d212
dsbnd <dashabond07@mail.ru>
2026-02-27 11:16:15 +0300

    auth

--- src/main/java/com/rzd/dispatcher/model/entity/User.java
+++ src/main/java/com/rzd/dispatcher/model/entity/User.java
@@ -1,12 +1,17 @@
 package com.rzd.dispatcher.model.entity;
 
+import com.rzd.dispatcher.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.OffsetDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -14,7 +19,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements org.springframework.security.core.userdetails.UserDetails {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
@@ -36,8 +41,32 @@ public class User {
     @Column(name = "created_at")
     private OffsetDateTime createdAt;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.USER;
+
     @PrePersist
     protected void onCreate() {
         createdAt = OffsetDateTime.now();
     }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    public String getPassword() {
+        return passwordHash; // Spring Security будет использовать наше поле passwordHash
+    }
+
+    public String getUsername() {
+        return email; // В качестве логина используем email
+    }
+
+    public boolean isAccountNonExpired() { return true; }
+
+    public boolean isAccountNonLocked() { return true; }
+
+    public boolean isCredentialsNonExpired() { return true; }
+
+    public boolean isEnabled() { return true; }
 }
