80884971c0236943d25589dc64a1e92294315a88
dsbnd <dashabond07@mail.ru>
2026-03-26 16:59:53 +0300

    conf

--- src/main/java/com/rzd/dispatcher/security/SecurityConfig.java
+++ src/main/java/com/rzd/dispatcher/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) //не используем куки для авторизация
+                .csrf(AbstractHttpConfigurer::disable)
 
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new CorsConfiguration();
