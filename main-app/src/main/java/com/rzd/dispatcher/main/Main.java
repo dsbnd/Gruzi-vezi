43996d07ef02ef7b7c38508acf9d24fee1e21998
dsbnd <dashabond07@mail.ru>
2026-05-12 20:43:12 +0300

    Huze nekuda

--- main-app/src/main/java/com/rzd/dispatcher/main/Main.java
+++ main-app/src/main/java/com/rzd/dispatcher/main/Main.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
 @EnableScheduling
 @EnableJpaRepositories(basePackages = "com.rzd.dispatcher.common.repository")
